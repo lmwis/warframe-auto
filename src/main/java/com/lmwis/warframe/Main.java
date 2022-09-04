93104613d980ce488a034331fe335348e98289ec
lmwis <detectiveygn@163.com>
2022-09-04 22:37:50 +0800

    feat: qumozhiren

--- src/main/java/com/lmwis/warframe/Main.java
+++ src/main/java/com/lmwis/warframe/Main.java
@@ -1,18 +1,63 @@
 package com.lmwis.warframe;
 
 import java.awt.*;
+import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 
 public class Main {
+
+    static Robot robot;
+
+    static {
+        try {
+            robot = new Robot();
+        } catch (AWTException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static void main(String[] args) throws AWTException {
         System.out.println("123");
-        Robot robot = new Robot();
-        robot.keyPress(KeyEvent.VK_A);
-//        robot.delay(100);
-//        robot.keyRelease(KeyEvent.VK_A);
-        robot.delay(1000);
-        robot.keyPress(KeyEvent.VK_B);
-//        robot.delay(100);
-//        robot.keyRelease(KeyEvent.VK_B);abab
+        int i=0;
+
+        while(i++<220){
+//           moveRound(i+200);ee
+            qumozhiren();
+//            i+= 0.1;
+        }
+        ;
+
+
+
+    }
+    public static void moveRound(int pix){
+        robot.mouseMove(pix,200);
+        robot.delay(500);
+        robot.mouseMove(pix,200);
+//        robot.delay(200);
+//        robot.mouseMove(200,200);
+
+
+    }
+    public static void leftClick(int ms){
+        robot.mousePress(KeyEvent.BUTTON1_DOWN_MASK);
+        robot.delay(200);
+        robot.mouseRelease(KeyEvent.BUTTON1_DOWN_MASK);
+        robot.delay(2000);
+
+    }
+
+    public static void qumozhiren(){
+        // 蓄力发射eee
+        robot.keyPress(KeyEvent.VK_E);
+        robot.delay(800);
+        robot.keyRelease(KeyEvent.VK_E);
+
+        robot.delay(400);
+
+        // 引爆
+        robot.mousePress(InputEvent.BUTTON2_DOWN_MASK);
+        robot.delay(100);
+        robot.mouseRelease(InputEvent.BUTTON2_DOWN_MASK);
     }
 }
